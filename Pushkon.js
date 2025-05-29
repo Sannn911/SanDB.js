98160ecb89495882c4594ba24862c5248f569f39
Sannn911 <san020112601@gmail.com>
2025-05-29 18:49:32 +0700

    Create Pushkon.js

---
+++ Pushkon.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "SANNNNID",
+
+  "description": "Create by TradzID", 
+
+  "lastUpdated": "14/05/2025",
+
+  "author": "TradzID", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "6.0"
+
+    }
